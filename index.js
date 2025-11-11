eccbba4a3c4eb10d5eb8df093c918dd87ea4fa2a
LJBH-OD-P-0045 <y.takahashi@plusplus.jp>
2025-11-11 15:42:40 +0900

    環境を更新。

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // 画面の準備ができたら...
-jQuery(function($){
+jQuery(function($) {
 
-  window.onhashchange = function(){
+  window.onhashchange = function() {
     showContents();
   }
   showContents();
-  showMenu()
+  showMenu();
 
   // ★marked.js の設定
   marked.setOptions({
@@ -13,17 +13,17 @@ jQuery(function($){
     breaks : true,
   });
 
-  function showContents(){
+  function showContents() {
 // add (S) スクロール位置をトップに戻す。
     $(window).scrollTop(0);
 // add (E)
-    var url = location.hash.replace('#', '');
-    if(url){
+    let url = location.hash.replace('#', '');
+    if (url) {
       $.ajax({
         url: url,
         cache: false,
-        success: function(md){
-          var html = marked(md);
+        success: function(md) {
+          let html = marked.parse(md);
           $('.st-MainContents').html(html);
 /*
 // css 用に変換する。
@@ -36,20 +36,30 @@ for(var i in mdEls){
     mdEl.parentNode.replaceChild(converted, mdEl);
 }
 */
+        },
+        error: function(xhr, status, err) {
+          $('.st-MainContents').html(`<p>⚠️ コンテンツを読み込めませんでした。</p>`);
         }
       });
     }
   }
 
   function showMenu(){
+    // google 翻訳での表示対策が必要。
+    // 元サイトへの AJAX 通信が CORS（Cross-Origin Resource Sharing）でブロックされると400。
+    // https://3tene-github-io.translate.goog/WebDocument/menu.md は 404。
+    let baseDir = '';
     // Chrome ローカルファイルにAjaxリクエストを送るとエラーになる。仕様らしい。
     // ※ ForeFox と EDGE は動作する。
     $.ajax({
-      url: 'menu.md',
+      url: baseDir + 'menu.md',
       cache: false,
-      success: function(md){
-        var html = marked(md);
+      success: function(md) {
+        let html = marked.parse(md);
         $('.st-SubMenu').html(html);
+      },
+      error: function(xhr, status, err) {
+        $('.st-SubMenu').html(`<p>⚠️ メニューを読み込めませんでした。1-${baseDir}</p>`);
       }
     });
   }
